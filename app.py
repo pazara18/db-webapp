d460a56f86397f21be9708dcf55f97eb32372097
Abdulkadir Pazar <pazara18@itu.edu.tr>
2021-01-15 22:36:04 +0300

    basic layout of the site and home page

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ else:
     app.debug = False
     DATABASE_URI = 'postgres://xmumbtkwyfnwjw:f6b558df185eb87ebb43097a973d453d72e56112d28431d9867ab967ad686ef9@ec2-34-230-149-169.compute-1.amazonaws.com:5432/d2p706ei2cossa'
 
-
-
-
 @app.route("/")
 def home_page():
-    return render_template('layout.html')
+    return render_template('home.html')
+
+@app.route("/contact-us")
+def contact_us_page():
+    return render_template('contact-us.html')
 
 
 if __name__ == "__main__":
-    app.debug = True
     app.run()
